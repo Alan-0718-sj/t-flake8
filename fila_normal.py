f30b81db412a21173c94e8183ef3e410efe33d1a
Alan <analisedesistemas20@gmail.com>
2023-08-07 10:39:30 -0300

    criando fila base

--- fila_normal.py
+++ fila_normal.py
@@ -1,15 +1,17 @@
 from fila_base import FilaBase
 
+
 class FilaNormal(FilaBase):
 
-    def gerasenhaatual(self)->None:
+    def gerasenhaatual(self) -> None:
         self.senhaatual = f'NM{self.codigo}'
 
-    def atualizafila(self)->None:
-        self.resetafila()
+    def atualizafila(self) -> None:
+        self.reseta_fila()
         self.gerasenhaatual()
         self.fila.append(self.senhaatual)
-    def chamacliente(self, caixa:int)->str:
-        cliente_atual:str = self.fila.pop(0)
-        self.clientesatendidos.append(cliente_atual)
+
+    def chamacliente(self, caixa: int) -> str:
+        cliente_atual: str = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
         return(f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,15 +1,16 @@
 from fila_base import FilaBase
 
+
 class FilaPrioritaria(FilaBase):
-    def gera_senha_atual(self)-> None:
+    def gera_senha_atual(self) -> None:
         self.senha_atual = f'PR{self.codigo}'
 
-    def atualiza_fila(self)-> None:
+    def atualiza_fila(self) -> None:
         self.reseta_fila()
         self.gera_senha_atual()
         self.fila.append(self.senha_atual)
 
-    def chama_cliente(self, caixa:int)-> str:
+    def chama_cliente(self, caixa: int) -> str:
         cliente_atual = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return (f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
